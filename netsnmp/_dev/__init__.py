fe54176a4676dbc5148a857e590616595c578d59
Gabe <optimum.reflex@gmail.com>
2015-12-07 00:08:44 -0500

    latest dev work

--- netsnmp/_dev/__init__.py
+++ netsnmp/_dev/__init__.py
@@ -25,16 +25,15 @@ _DEVTYPES = {
 class SNMPDevice(object):
     # Default OIDs
     _oid_mapping = {
-        'sysDescr': '1.3.6.1.2.1.1.1.0',
-        'sysUptime': '1.3.6.1.2.1.1.3.0',
+        'sysDescr': '.1.3.6.1.2.1.1.1.0',
+        'sysUptime': '.1.3.6.1.2.1.1.3.0',
     }
-    _inverted_oid_mapping = {}
 
     def __init__(self, maxoids=None):
         # Request attributes
         self.maxoids = maxoids
         self.oids = list(self._oid_mapping.values())
-        self._inverted_oid_mapping = {v: k for k, v in self._oid_mapping.items()}
+        self._inverted_oid_mapping = {v: k for (k, v) in self._oid_mapping.items()}
         # Response attributes
         self.response = None
         self.typestr = None
@@ -42,11 +41,11 @@ class SNMPDevice(object):
 
     def parse_oids(self, response):
         vars = [var.split('=', maxsplit=1) for var in response]
-        return {oid: value for (oid, value) in vars}
+        return (len(vars), {self._inverted_oid_mapping[oid]: value for (oid, value) in vars})
 
 class SNMPDevice_model1(SNMPDevice):
     _oid_mapping = {
-        'sysDescr': '1.3.6.1.2.1.1.1.0',
+        'sysDescr': '.1.3.6.1.2.1.1.1.0',
     }
 
     def __init__(self):
@@ -54,7 +53,7 @@ class SNMPDevice_model1(SNMPDevice):
 
 class SNMPDevice_model2(SNMPDevice):
     _oid_mapping = {
-        'sysUptime': '1.3.6.1.2.1.1.3.0',
+        'sysUptime': '.1.3.6.1.2.1.1.3.0',
     }
 
     def __init__(self):
@@ -62,7 +61,7 @@ class SNMPDevice_model2(SNMPDevice):
 
 class SNMPDevice_model3(SNMPDevice):
     _oid_mapping = {
-        'test1': '1.3.6.1.2.1.1.5.0',
+        'test1': '.1.3.6.1.2.1.1.5.0',
     }
 
     def __init__(self):
@@ -70,7 +69,8 @@ class SNMPDevice_model3(SNMPDevice):
 
 class SNMPDevice_model4(SNMPDevice):
     _oid_mapping = {
-        'test2': '1.3.6.1.2.1.1.7.0',
+        'test2': '.1.3.6.1.2.1.1.7.0',
+        'test3': '.1.3.6.1.2.1.1.11.0',
     }
 
     def __init__(self):
@@ -78,17 +78,17 @@ class SNMPDevice_model4(SNMPDevice):
 
 class SNMPDevice_model5(SNMPDevice):
     _oid_mapping = {
-        'test3': '1.3.6.1.2.1.1.9.0',
+        'test4': '.1.3.6.1.2.1.1.9.0',
     }
 
     def __init__(self):
         SNMPDevice.__init__(self)
 
 SNMP_DEVTYPES = {
-    'model1': SNMPDevice_model1(),
-    'model2': SNMPDevice_model2(),
-    'model3': SNMPDevice_model3(),
-    'model4': SNMPDevice_model4(),
+#    'model1': SNMPDevice_model1(),
+#    'model2': SNMPDevice_model2(),
+#    'model3': SNMPDevice_model3(),
+#    'model4': SNMPDevice_model4(),
 #    'model5': SNMPDevice_model5(),
     '__default__': SNMPDevice(),
 }
