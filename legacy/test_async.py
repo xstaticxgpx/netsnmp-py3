f7763971aaf70084594526b259b113adb6a840c0
Gabe <root@un1x.su>
2015-12-17 07:51:41 +0000

    new test_async, many improvements

--- legacy/test_async.py
+++ legacy/test_async.py
@@ -2,11 +2,11 @@
 
 from netsnmp._api import get_async
 from async_devtypes import SNMP_DEVTYPES
-import cx_Oracle
+#import cx_Oracle
 import zmq
 import redis, hiredis
 import logging, logging.handlers
-import queue, random, sys, threading, time
+import queue, random, sys, time
 import multiprocessing as mp
 
 # command name
@@ -29,7 +29,7 @@ DB = {
         'port'  : '1521',
         'name'  : 'name',
         'user'  : 'user',
-        'pass'  : 'pass',
+        'pass'  : 'pass'
     },
 }
 
@@ -38,12 +38,11 @@ DB = {
 # limitation lies somewhere between 49112 and 49116(??) - getting EPERM (1)... kernel dropping?
 #http://comments.gmane.org/gmane.comp.security.firewalls.netfilter.devel/29993
 # Ceiling is around 8 * 4096 = 32768 max requests at any given time
-# or 4096*12 = 3072*16 = 49152
-MAX_WORKERS = 16
-MAX_PER_WORKER = 3072
+MAX_WORKERS = int(mp.cpu_count()*2) if mp.cpu_count() <= 4 else 8
+MAX_PER_WORKER=4096
 
 # Number of ZMQ PULL processes to spawn
-ZMQ_PROCESSORS = 8
+ZMQ_PROCESSORS = int(MAX_WORKERS/2)
 
 # Time to pause for ZMQ initialization (Seconds)
 ZMQ_PAUSE=0.01
@@ -56,9 +55,9 @@ ZMQ_IN  = "ipc:///tmp/%s_in" % cmdname
 ZMQ_OUT = "ipc:///tmp/%s_out" % cmdname
 
 ## SNMP
-# SNMP timeout is in milliseconds
-SNMP_TIMEOUT=1250
 SNMP_RETRIES=1
+# SNMP timeout is in milliseconds
+SNMP_TIMEOUT=500
 SNMP_TIMEOUT_DELTA=MAX_WORKERS
 
 # Response callback message intake/processing
@@ -71,64 +70,72 @@ def ZMQProcessor(success, timeout, oidcount):
     HOST=1
     DEVTYPE=2
     OIDS=3
-    # Local counters, rolled up into mp.Value at end
+
     _type_count = {}
+    # Local counters, rolled up into mp.Value at end
     _success = 0
     _timeout = 0
     _oidcount = 0
+
+
+    # Per-processor queue
+    q = queue.Queue()
 
     # Processor ZMQ PULL socket
     incoming = zmq.Context().socket(zmq.PULL)
     incoming.setsockopt(zmq.RCVHWM, ZMQ_HWM)
     incoming.connect(ZMQ_OUT)
 
+    # redis pipeline
+    #_redis = redis.Redis(host='127.0.0.1').pipeline()
+
     log.debug('Starting up...')
 
-    ## Intake
+    # Pull via ZMQ and Queue
+    while True:
+        response = incoming.recv_multipart()
+        if response[OP] == b'_sentinel':
+            log.debug('Shutting down ZMQ_PULL socket...')
+            incoming.close()
+            break
+        q.put_nowait(response)
 
-    _redis = redis.Redis(host='127.0.0.1')
-    with _redis.pipeline() as _redis:
-        i=0
-        # Pull via ZMQ and Queue
-        while True:
-            response = [frame.decode() for frame in incoming.recv_multipart()]
-            if response[OP] == '_sentinel':
-                log.debug('Shutting down ZMQ_PULL socket...')
-                incoming.close()
-                break
+    # Queue processing
+    qsize = q.qsize()
+    log.debug('Processing %d responses...' % (qsize,))
+    start = time.perf_counter()
+    i=0
+    while not q.empty():
+        response = [frame.decode() for frame in q.get()]
+        if response[OP] == '1':
+            _success+=1
 
-            elif response[OP] == '1':
-                _success+=1
- 
-                try:
-                    _type_count[response[DEVTYPE]]+=1
-                except KeyError:
-                    # Need to define
-                    _type_count[response[DEVTYPE]]=1
+            try:
+                _type_count[response[DEVTYPE]]+=1
+            except KeyError:
+                # Need to define
+                _type_count[response[DEVTYPE]]=1
 
-                # Parse OIDs
-                _vars = SNMP_DEVTYPES[response[DEVTYPE]].parse_oids(response[OIDS:])
-                _oidcount+=len(_vars)
-                #log.debug(_vars)
-                try: 
-                    #log.debug("%s [%s] %s", response[HOST], response[DEVTYPE], vars)
-                    # splice if ipv6
-                    _redis.hmset(response[HOST] if not response[HOST][:4]=="udp6" else response[HOST][6:][:-1],
-                                 _vars)
-                    i+=1
-                except redis.exceptions.RedisError as e:
-                    log.debug('redis exception: %s %s:%s', str(e).strip(), response[HOST], _vars)
-                    continue
-                # Flush redis pipeline periodically
-                if i > 4096:
-                    _redis.execute()
-                    i=0
+            # Parse OIDs
+            _vars = SNMP_DEVTYPES[response[DEVTYPE]].parse_oids(response[OIDS:])
+            _oidcount+=len(_vars)
+            #log.debug(_vars)
+            #try: 
+                #log.debug("%s [%s] %s", response[HOST], response[DEVTYPE], vars)
+                #_redis.hmset(response[HOST] if not response[HOST].startswith("udp6") else response[HOST].replace("udp6:[", "").replace("]", ""),
+                #             vars)
+                #i+=1
+            #except redis.exceptions.RedisError as e:
+                #log.debug('redis exception: %s' % (str(e).strip()))
+                #continue
+            # Flush redis pipeline periodically
+            #if i > 4095:
+                #_redis.execute()
+                #i=0
+        elif response[OP] == '2':
+            _timeout+=1
 
-            elif response[OP] == '2':
-                _timeout+=1
-
-        _redis.execute()
-
+    #_redis.execute()
     # Counter rollup
     with success.get_lock():
         success.value+=_success
@@ -137,9 +144,10 @@ def ZMQProcessor(success, timeout, oidcount):
     with oidcount.get_lock():
         oidcount.value+=_oidcount
 
-    #log.info('Finished processing %d responses in %.3fs' % (qsize, elapsed))
-    log.info('Processed %d responses', (_success+_timeout))
-    log.debug('%s', _type_count)
+    end = time.perf_counter()
+    elapsed = end-start
+    log.info('Finished processing %d responses in %.3fs' % (qsize, elapsed))
+    log.info('%s', _type_count)
 
 # Messaging Pipeline
 def ZMQStreamer(running):
@@ -162,8 +170,8 @@ def ZMQStreamer(running):
 
 if __name__ == '__main__':
 
-    db = DB['db']
-    dbh = cx_Oracle.connect('%s/%s@%s/%s' % (db['user'], db['pass'], db['server'], db['name']))
+    #db = DB['db']
+    #dbh = cx_Oracle.connect('%s/%s@%s/%s' % (db['user'], db['pass'], db['server'], db['name']))
 
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s.%(msecs)03dZ [%(processName)s/%(levelname)s] %(message)s',
@@ -179,25 +187,20 @@ if __name__ == '__main__':
     # Overwrite handlers to only utilize QueueHandler()
     log.handlers = [log_async,]
 
-    select = dbh.cursor()
-    select.arraysize = 4096
-
-    query = "select something from somewhere"
-    community = "public"
-
+    hosts = []
     # Absolute start timer
     _start = time.time()
     # Step timer
     start = time.perf_counter()
-    #list(tuple(peername str, community str, devtype str, devtype class instance)..)
-    hosts = [(
-         "udp6:["+host[CM_IP]+"]" if host[CM_IP][4]==':' else host[CM_IP],
-         community,
-         host[MODEL] if host[MODEL] in SNMP_DEVTYPES else '__other__',
-         SNMP_DEVTYPES[host[MODEL]] if host[MODEL] in SNMP_DEVTYPES else SNMP_DEVTYPES['__other__']
-         ) for host in select.execute(query)]
-    select.close()
-    dbh.close()
+    [hosts.append(
+        #peername str, community str, devtype str, devtype class instance
+        #(host, 'public', '__default__', SNMP_DEVTYPES['__default__'])
+        (host, 'public', host, SNMP_DEVTYPES[host])
+        #(host, community, 'archt', SNMPDevice_archt)
+    ) for host in ('archt01', 'archt02', 'archt03', 'archt04', 'archt05')*10000]
+    #log.debug(hosts)
+    #select.close()
+    #dbh.close()
     total = len(hosts)
     end = time.perf_counter()
     log.info('got %d hosts from DB in %.3fms' % (total, (end-start)*1000))
@@ -248,11 +251,12 @@ if __name__ == '__main__':
         start = time.perf_counter()
         _timeout = SNMP_TIMEOUT
         while hosts or active_workers:
+            pids = []
             remaining = total-i
             if hosts and len(active_workers) < MAX_WORKERS:
-                #_timeout = SNMP_TIMEOUT+random.randint(p%2, SNMP_TIMEOUT_DELTA)
+                _timeout = int(SNMP_TIMEOUT+random.randint(p%2, SNMP_TIMEOUT_DELTA))
                 _upper = i+MAX_PER_WORKER if remaining > MAX_PER_WORKER else i+remaining
-                log.debug('Defining process for range %d:%d (%d*%dms)', i, _upper, (SNMP_RETRIES+1), _timeout)
+                log.debug('Defining process for range %d:%d (%dms)' % (i, _upper, _timeout))
 
                 # Define process(es) which call get_async C function
                 # get_async([(str hostname, str community, [str oid,..])..], int timeout_ms, int retries, int ZMQ_HWM, str ZMQ_IN)
@@ -270,13 +274,11 @@ if __name__ == '__main__':
                 i+=MAX_PER_WORKER
                 if len(active_workers) < MAX_WORKERS:
                     p+=1
-                    # test, slow initial startup
-                    time.sleep((SNMP_TIMEOUT/MAX_WORKERS)/1000)
                     continue
                 else:
                     p=0
 
-            pids = [proc.pid for proc in active_workers]
+            [pids.append(proc.pid) for proc in active_workers]
             log.debug('Process PIDs: %s' % pids)
 
             # Continually monitor progress and re-loop when all workers are finished, or maintain process pool with force_reloop
@@ -311,14 +313,12 @@ if __name__ == '__main__':
         elapsed = end-start
         log.info('Polling completed in %.3fs' % elapsed)
 
-        __start = time.perf_counter()
-        log.debug('Waiting for ZMQProcessors...')
         # Signal end to ZMQProcessor(s)
         zmq_sentinel = zmq.Context().socket(zmq.PUSH)
         zmq_sentinel.connect(ZMQ_IN)
         for _ in range(ZMQ_PROCESSORS):
             zmq_sentinel.send(b'_sentinel')
-            # This may help ensure each send above reaches a unique ZMQ_PROCESSOR
+            # This may help ensure each send above reaches a unique ZMQ_PROCCESSOR
             time.sleep(0.001)
         zmq_sentinel.close()
 
@@ -327,9 +327,6 @@ if __name__ == '__main__':
             for zmq_processor in zmq_processors:
                 zmq_processor.join()
                 zmq_processors.remove(zmq_processor)
-
-        __elapsed = time.perf_counter()-__start
-        log.debug('ZMQProcessors took an additional %.3fs', __elapsed)
 
         log.info('from %d total hosts got %d timeouts (%.1f%%)' % (total, timeout.value, ((timeout.value/total)*100)))
         log.info('got %d oid responses from %d hosts' % (oidcount.value, success.value))
